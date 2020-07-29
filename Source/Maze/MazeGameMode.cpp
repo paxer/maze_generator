6f3894d80d50685057a5240070bcc37c4747ade6
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2020-07-29 20:07:28 +1000

    build simple maze

--- Source/Maze/MazeGameMode.cpp
+++ Source/Maze/MazeGameMode.cpp
@@ -4,10 +4,7 @@
 #include "MazeCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
-AMazeGameMode::AMazeGameMode()
-	: Super()
+AMazeGameMode::AMazeGameMode() : Super()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+    
 }
---
+++ Source/Maze/Wall.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Wall.h"
+
+// Sets default values
+AWall::AWall()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	RootComponent = Mesh;
+
+}
+
+// Called when the game starts or when spawned
+void AWall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
